be8a39203521bd24fca125dd9e1b4c7eaff6ac4b
Jiaaming <2455951489@qq.com>
2024-11-28 13:05:14 -0800

    dix: import bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cli-snapsort',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         "click>=8.1.0",
